e38555b11e3a8a20b84509a13ddf44f3fb8fb50f
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-02-23 20:02:29 +0100

    connexion à mongoDB
    connexion à mongoDB

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// mongodb+srv://mathiasteddy:<password>@cluster0.uze0as9.mongodb.net/?retryWrites=true&w=majority
+
+mongoose.connect('mongodb+srv://mathiasteddy:bozptpnGdmEixPHx@cluster0.uze0as9.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
 
-// middleware
+// intercepte les requetes avec du json
+app.use(express.json());
+
+// middleware : https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466298-creez-une-route-get
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.post('/api/stuff', (req, res, next) => {
+console.log(req.body);
+res.status(201).json({
+    message : 'Objet créé'
+});
 });
 
-// middleware
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-// middleware
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-// middleware
-app.use((req, res) => {
-  console.log('Réponse envoyée avec succès !');
-});
+// middleware 
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
