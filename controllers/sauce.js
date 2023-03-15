b668ecd83f27cd455e3823597d5f73cac8d859a3
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-03-15 09:32:36 +0100

    models sauce vérification
    models sauce vérification

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,27 +1,5 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
-// exports.createSauce = (req, res, next) => {
-//   const sauce = new Sauce({
-//     title: req.body.title,
-//     description: req.body.description,
-//     imageUrl: req.body.imageUrl,
-//     price: req.body.price,
-//     userId: req.body.userId
-//   });
-//   sauce.save().then(
-//     () => {
-//       res.status(201).json({ 
-//         message: 'Post saved successfully!'
-//       });
-//     }
-//   ).catch(
-//     (error) => {
-//       res.status(400).json({
-//         error: error
-//       });
-//     }
-//   );
-// };
 
 exports.create = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
@@ -54,30 +32,6 @@ exports.getOne = (req, res, next) => {
   );
 };
 
-// exports.modifySauce = (req, res, next) => {
-//   const sauce = new Sauce({
-//     _id: req.params.id,
-//     title: req.body.title,
-//     description: req.body.description,
-//     imageUrl: req.body.imageUrl,
-//     price: req.body.price,
-//     userId: req.body.userId
-//   });
-//   Sauce.updateOne({_id: req.params.id}, sauce).then(
-//     () => {
-//       res.status(201).json({
-//         message: 'Sauce updated successfully!'
-//       });
-//     }
-//   ).catch(
-//     (error) => {
-//       res.status(400).json({
-//         error: error
-//       });
-//     }
-//   );
-// };
-
 exports.modify = (req, res, next) => {
   const sauceObject = req.file ? {
     ...JSON.parse(req.body.sauce),
@@ -99,22 +53,6 @@ exports.modify = (req, res, next) => {
       res.status(400).json({ error });
     });
 };
-
-// exports.deleteSauce = (req, res, next) => {
-//   Sauce.deleteOne({_id: req.params.id}).then(
-//     () => {
-//       res.status(200).json({
-//         message: 'Deleted!'
-//       });
-//     }
-//   ).catch(
-//     (error) => {
-//       res.status(400).json({
-//         error: error
-//       });
-//     }
-//   );
-// };
 
 exports.delete = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
--- models/sauce.js
+++ models/sauce.js
@@ -6,12 +6,12 @@ const sauceSchema = mongoose.Schema({
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
-  imageUrl: { type: String },
-  heat: { type: Number, required: true },
-  likes: { type: Number },
-  dislikes: { type: Number },
-  usersLiked: { type: Array },
-  usersDisliked: { type: Array },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true, min: 1, max: 10 },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: { type: Array, default: [] },
+  usersDisliked: { type: Array, default: [] },
 });
 
-module.exports = mongoose.model("Sauce", sauceSchema);
+module.exports = mongoose.model("sauce", sauceSchema);
